42e7c66698d390b9b6b71db5dbd58a99726cd665
kr9157006 <kr9157006@gmail.com>
2024-02-19 13:23:23 +0300

    sffd

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,32 +1,29 @@
 <?php
 
-
 namespace App\Http\Controllers\Admin;
 
-
 // use DB;
+use App\Http\Controllers\Controller;
 use App\Models\ChangeUserNameModel;
 use App\Models\User;
-use Illuminate\View\View;
-use Illuminate\Support\Arr;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 // use Hash;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    function __construct()
-    {
-        $this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index', 'store']]);
-        $this->middleware('permission:user-create', ['only' => ['create', 'store']]);
-        $this->middleware('permission:user-edit', ['only' => ['edit', 'update']]);
-        $this->middleware('permission:user-delete', ['only' => ['destroy']]);
-    }
+    // function __construct()
+    // {
+    //     $this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index', 'store']]);
+    //     $this->middleware('permission:user-create', ['only' => ['create', 'store']]);
+    //     $this->middleware('permission:user-edit', ['only' => ['edit', 'update']]);
+    //     $this->middleware('permission:user-delete', ['only' => ['destroy']]);
+    // }
     /**
      * Display a listing of the resource.
      *
@@ -35,10 +32,10 @@ class UserController extends Controller
     public function index(Request $request): View
     {
         $data = User::latest()->paginate(5);
+
         return view('admin.users.index', compact('data'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
-
 
     /**
      * Show the form for creating a new resource.
@@ -48,14 +45,13 @@ class UserController extends Controller
     public function create(): View
     {
         $roles = Role::pluck('name', 'name')->all();
+
         return view('admin.users.create', compact('roles'));
     }
-
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request): RedirectResponse
@@ -64,7 +60,7 @@ class UserController extends Controller
             'user_name' => 'required',
             'email' => 'required|email|unique:web_user,email',
             'password' => 'required|same:confirm-password',
-            'roles' => 'required'
+            'roles' => 'required',
         ]);
         $input = $request->all();
 
@@ -73,25 +69,25 @@ class UserController extends Controller
 
         $input['password'] = Hash::make($input['password']);
 
-
         $user = User::create($input);
         $user->assignRole($request->input('roles'));
-
 
         return redirect()->route('users.index')
             ->with('success', 'User created successfully');
     }
 
-    public function changeUserName(){
+    public function changeUserName()
+    {
         $chunModel = new ChangeUserNameModel();
+
         return view('admin.users.change_user_name', compact('chunModel'));
     }
 
-    public function updateUserName(Request $request){
+    public function updateUserName(Request $request)
+    {
         // $chunModel = new ChangeUserName();
         // return view('admin.users.change_user_name', compact('user'));
     }
-
 
     /**
      * Display the specified resource.
@@ -102,9 +98,9 @@ class UserController extends Controller
     public function show($id): View
     {
         $user = User::find($id);
+
         return view('admin.users.show', compact('user'));
     }
-
 
     /**
      * Show the form for editing the specified resource.
@@ -118,15 +114,12 @@ class UserController extends Controller
         $roles = Role::pluck('name', 'name')->all();
         $userRole = $user->roles->pluck('name', 'name')->all();
 
-
         return view('admin.users.edit', compact('user', 'roles', 'userRole'));
     }
-
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -139,27 +132,22 @@ class UserController extends Controller
         //     'roles' => 'required'
         // ]);
 
-
         $input = $request->all();
-        if (!empty($input['password'])) {
+        if (! empty($input['password'])) {
             $input['password'] = Hash::make($input['password']);
         } else {
-            $input = Arr::except($input, array('password'));
+            $input = Arr::except($input, ['password']);
         }
-
 
         $user = User::find($id);
         $user->update($input);
         DB::table('model_has_roles')->where('model_id', $id)->delete();
 
-
         $user->assignRole($request->input('roles'));
-
 
         return redirect()->route('admin.users.index')
             ->with('success', 'User updated successfully');
     }
-
 
     /**
      * Remove the specified resource from storage.
@@ -170,6 +158,7 @@ class UserController extends Controller
     public function destroy($id): RedirectResponse
     {
         User::find($id)->delete();
+
         return redirect()->route('admin.users.index')
             ->with('success', 'User deleted successfully');
     }
