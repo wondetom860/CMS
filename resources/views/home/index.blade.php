c9ec331b70a000a7cb256d37c8aae26640623cbc
wondetom860 <wondimu860@gmail.com>
2024-02-26 14:11:20 +0300

    cases registered today

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -40,7 +40,8 @@
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="admin/courtstaff" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="admin/courtstaff" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
 
@@ -60,7 +61,21 @@
 
         </div>
 
-        <div class="row"></div>
+        <div class="row">
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ App\Models\CaseModel::getTodayRegisteredCases() }}</h3>
+                        <p>Cases registered today</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
 
     </div>
 @stop
